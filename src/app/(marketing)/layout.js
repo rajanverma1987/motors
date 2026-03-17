5199e8a862dd3b3a5fcc8ffb4d56b083913c655d
rajan <rajanverma1987@gmail.com>
2026-03-17 15:36:57 +0530

    clarity

--- src/app/(marketing)/layout.js
+++ src/app/(marketing)/layout.js
@@ -25,9 +25,17 @@ gtag('config', '${GA_MEASUREMENT_ID}');
         }}
       />
       <Script
-        id="microsoft-clarity"
-        src={`https://www.clarity.ms/tag/${CLARITY_PROJECT_ID}`}
+        id="clarity-init"
         strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+(function(c,l,a,r,i,t,y){
+  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "${CLARITY_PROJECT_ID}");
+          `.trim(),
+        }}
       />
       <Navbar />
       <main className="flex-1">{children}</main>
